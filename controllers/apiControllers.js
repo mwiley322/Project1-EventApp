9cda248476725ff5636dccc0779c6e135570df6e
Megan Wiley <mwiley322@gmail.com>
2017-03-13 10:35:47 -0700

    Create all endpoints in apiControllers

---
+++ controllers/apiControllers.js
@@ -0,0 +1,30 @@
+function index(req, res) {
+  res.json({
+    message: 'Welcome to TechSpace! Your main hub for local tech events',
+    documentation_url: 'https://github.com/mwiley322/Project1-EventApp',
+    base_url: 'localhost:3000',
+    endpoints: [
+      {
+        method: 'GET', path: '/api', description: 'Describes available endpoints'
+      },
+      {
+        method: 'GET', path: '/api/events', description: 'Gets all events within the database'
+      },
+      {
+        method: 'GET', path: '/api/events/:id', description: 'Gets a single event based on parameters'
+      },
+      {
+        method: 'POST', path: '/api/events', description: 'Creates a single new event'
+      },
+      {
+        method: 'POST', path: '/api/events/:id', description: 'Updates a single existing event'
+      },
+      {
+        method: 'DELETE', path: '/api/events/:id', description: 'Destroys a single event'
+      }
+    ]//closes endpoints array
+  }); //closes json object to send
+} //closes index function
+
+//export for use elsewhere in app
+module.exports = {index: index};
