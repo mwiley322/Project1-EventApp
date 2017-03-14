3063f4fd05db63cf0ac55a70de29ecabe9de318c
Lamar Smith <DreamBig@Laillahx.local>
2017-03-14 14:06:45 -0700

    modalUpdate

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,6 +10,17 @@ $(document).ready(function() {
       error: handleError
     }); //closes ajax get request
 
+    $('#createEvent').on('click', function postNewEvent(e) {
+      e.preventDefault();
+      var formData = $(this).serialize(); //translates request
+      console.log('here is the form data: ', formData);
+        $.post('/api/events', formData, function(event) {
+          console.log('event to post: ', event);
+          renderAlbum(album);
+        }); //closes ajax post request
+    renderEvent(event);
+    }); //closes postNewEvent function
+
 }); //closes DOM ready function
 
 function renderMultipleEvents(events) {
@@ -19,6 +30,11 @@ function renderMultipleEvents(events) {
 }//closes rendermult.
 
 function renderEvent(event, keyword) {
+  var keyWordArray = event.keywords;
+  keyWordArray = keyWordArray.map( function ripActualKeywordsOut(keyWord){
+    return keyWord.name;
+  });
+  event.keywords = keyWordArray.join(', ');
   var eventHtml = (`
     <div class="row event">
       <div class="col-md-10 col-md-offset-1">
@@ -26,32 +42,90 @@ function renderEvent(event, keyword) {
           <div class="panel-body">
           <!-- begin event internal row -->
             <div class='row'>
-              <div class="col-md-3 col-xs-12 thumbnail event-art">
-                <img src="images/800x800.png" alt="event image">
+              <div class="col-lg- col-md-3 col-xs-12 thumbnail event-art">
+                <img src="${event.imageUrl}" class="responsive-img" alt="event image">
               </div>
               <div class="col-md-9 col-xs-12">
                 <ul class="list-group">
                   <li class="list-group-item">
-                    <h4 class='inline-header'>Event Name:</h4>
-                    <span class='event-name'>${event.eventName}</span>
+                    <h4 class='inline-header'>${event.eventName}</h4>
                   </li>
                   <li class="list-group-item">
-                    <h4 class='inline-header'>Description:</h4>
+                    <span class='eventLocation'>${event.location}</span>
+                    <span class='eventTime pull-right'>&#160;${event.time}</span>
+                    <span class='eventDate pull-right'>${event.date}</span>
+
+                  </li>
+                  <li class="list-group-item">
                     <span class='eventDescription'>${event.description}</span>
                   </li>
                   <li class="list-group-item">
-                    <h4 class='inline-header'>Date</h4>
-                    <span class='event-date'>${event.date}</span>
+                    <span class='event-date'>${event.peopleInterested} people interested</span>
                   </li>
                   <li class="list-group-item">
                     <h4 class="inline-header">Keywords:</h4>
                     <span class='event-keywords'>${event.keywords}</span>
                   </li>
                 </ul>
+                <div class="col-xs-2">
+                  <button type="button" class="btn btn-xs btn-info pull-right" id="moreEventInfo" data-toggle="modal" data-target="#moreEventInfoModal">
+                    Learn more
+                  </button>
+                  <!-- Modal -->
+                  <div class="modal fade" id="moreEventInfoModal" tabindex="-1" role="dialog" aria-labelledby="moreEventInfoModalLabel">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        </div>
+                        <div class="modal-body">
+                          <form class="form-horizontal">
+                          <div class="row event">
+                            <div class="col-md-10 col-md-offset-1">
+                              <div class="panel panel-default">
+                                <div class="panel-body">
+                                <!-- begin event internal row -->
+                                  <div class='row'>
+                                    <div class="col-lg- col-md-3 col-xs-12 thumbnail event-art">
+                                      <img src="http://wp.streetwise.co/wp-content/blogs.dir/2/files/2015/12/Ladies_Learning_Code_event_November_26_2011-630x420.jpg" class="responsive-img" alt="event image">
+                                    </div>
+                                    <div class="col-md-9 col-xs-12">
+                                      <ul class="list-group">
+                                        <li class="list-group-item">
+                                          <h4 class='inline-header'>${event.eventName}</h4>
+                                        </li>
+                                        <li class="list-group-item">
+                                          <span class='eventLocation'>${event.location}</span>
+                                          <span class='eventTime pull-right'>&#160;${event.time}</span>
+
+                                          <span class='eventDate pull-right'>${event.date}</span>
+
+                                        </li>
+                                        <li class="list-group-item">
+                                          <span class='eventDescription'>Hello students! Our next event will be held at 1-5PM. Chime in on this issue to join us as a mentor or student for this event!</span>
+                                        </li>
+                                        <li class="list-group-item">
+                                          <span class='event-date'>19 people interested</span>
+                                        </li>
+                                        <li class="list-group-item">
+                                          <h4 class="inline-header">Keywords:</h4>
+                                          <span class='event-keywords'>${event.keywords}</span>
+                                        </li>
+                                      </ul>
+                        <div class="form-group modal-footer">
+                          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+
+                </div>
+              </div>
               </div>
             </div>
             <!-- end of event internal row -->
-            <div class='panel-footer'>
+
             </div>
           </div>
         </div>
