95985c81fb02ff4690c68aabe6624b960441bf37
Lamar Smith <DreamBig@Laillahx.local>
2017-03-13 18:57:42 -0700

    tweaked submit button

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -23,17 +23,47 @@ function show(req, res) {
 
 // POST /api/events
 function create(req, res) {
-  var newEvent = new Event(req.body);
-  db.Event.save(function (err, createdEvent) {
-    if (err) { console.log('err: ', err);
-      res.send(err); }
-    console.log('event created: ', createdEvent);
+  var newEvent = new db.Event(req.body);
+  db.Event.create(newEvent, function(err, createdEvent) {
+    if(err){return console.log(err);}
+    console.log('made new event: ', createdEvent);
     res.json(createdEvent);
-  }); //closes save function
+  });
 }//closes create function
+
+// PUT /api/events/:id
+function update(req, res) {
+  console.log('event update: ', req.params);
+  var eventId = req.params.id; //id to search
+  var eventToUpdate = req.body; //form data to update
+  db.Event.findByIdAndUpdate(eventId, eventToUpdate, {new: true}, function(err, updatedEvent) { //newtrue means that it sends back the updated version
+    if (err) { console.log('err!: ', err);
+      res.sendStatus(204);
+    } else {
+      console.log(updatedEvent);
+      res.json(updatedEvent);
+    } //closes else statement
+  }); //closes findByIdAndUpdate
+} //closes update function
+
+// DELETE /api/events/:id
+function destroy(req, res) {
+  console.log('event to delete:', req.params);
+  var eventId = req.params.id;
+  db.Event.findByIdAndRemove(eventId, function(err, deletedEvent) {
+    if (err) {
+      console.log('err! ', err);
+    } else {
+      res.json(deletedEvent);
+    } //closes else statement
+  });//closes findByIdAndRemove
+} //closes destroy function
+
 
 module.exports = {
   index: index,
   show: show,
-  create: create
+  create: create,
+  update: update,
+  destroy: destroy
 };
