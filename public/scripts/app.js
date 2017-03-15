c154a8e3bb5e9e63f1c1f7cc6c4f4e3a885f3afc
Megan Wiley <mwiley322@gmail.com>
2017-03-15 16:04:00 -0700

    File include's Lamar's google map work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,6 +22,10 @@ $(document).ready(function() {
         error: handleError
     }); //closes ajax get request
 
+    // Google Maps Start
+      // initMap();
+      // end of google maps
+
     $('#createEvent').on('click', handleNewEventSubmit);
 
     $('#eventSearchButton').on('click', handleSearchSubmit);
@@ -32,8 +36,6 @@ $(document).ready(function() {
     });
 
     $( function autoSearch() {
-
-
        $("#tags").autocomplete({
          minLength: 1,
          source: availableTags,
@@ -44,7 +46,7 @@ $(document).ready(function() {
             $(this).val(''); return false;
           }//closes select function
        });//closes autocomplete function
-  }); //closes search function
+    }); //closes search function
 
     $("#eventSearchForm").autocomplete({
       minLength: 1,
@@ -58,8 +60,77 @@ $(document).ready(function() {
     });//closes autocomplete function
 
 
+
 }); //closes DOM ready function
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -33.8688,
+      lng: 151.2195
+    },
+    zoom: 13
+  });
+  var input = /** @type {!HTMLInputElement} */ (
+    document.getElementById('pac-input'));
+    var types = document.getElementById('type-selector');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
+    });
+    autocomplete.addListener('place_changed', function() {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17); // Why 17? Because it looks good.
+      }
+      marker.setIcon( /** @type {google.maps.Icon} */ ({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+    });
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, types) {
+      var radioButton = document.getElementById(id);
+      radioButton.addEventListener('click', function() {
+        autocomplete.setTypes(types);
+      });
+    }
+    setupClickListener('changetype-all', []);
+    setupClickListener('changetype-address', ['address']);
+    setupClickListener('changetype-establishment', ['establishment']);
+    setupClickListener('changetype-geocode', ['geocode']);
+  }
 
 function renderMultipleEvents(events) {
   events.forEach(function(event) {
